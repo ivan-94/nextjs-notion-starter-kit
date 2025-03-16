765f5612da2239716fdde20d1336c253308c3bea
ivan <carney520@hotmail.com>
2025-03-16 20:52:21 +0800

    fix: 限制最新递归

--- lib/get-site-map.ts
+++ lib/get-site-map.ts
@@ -37,7 +37,10 @@ async function getAllPagesImpl(
   const pageMap = await getAllPagesInSpace(
     rootNotionPageId,
     rootNotionSpaceId,
-    getPage
+    getPage,
+    {
+      maxDepth: 2
+    }
   )
 
   const canonicalPageMap = Object.keys(pageMap).reduce(
